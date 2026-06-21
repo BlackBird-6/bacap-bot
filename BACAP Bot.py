914db131f06407a621318502d890273f34b4b5ea
BlackBird_6 <blackbird6366@gmail.com>
2026-06-20 23:10:29 -0400

    add README.md

--- BACAP Bot.py
+++ BACAP Bot.py
@@ -3,94 +3,6 @@
 Coded By: BlackBird_6, saladbowls, and Ktano2o6o8 
 Last Updated: 2026-06-20
 Current Version: v1.4.0
-
-A general-purpose discord bot to assist with playing BlazeandCave's Advancement Pack!
-Shows advancement names, rewards, requirements, and much much more!
-
-=== changelog v1.4 ===
-
-v1.4.0
-- Added the Unlocked Potential, New Horizons, Determination Edition and Slop Edition packs
-- Updated supported BACAP pack version to 1.21
-- Updated versions and documentation commands
-- Autocomplete fields now use fuzzy autocomplete with a score cutoff of 85% to allow for some typos
-- Added "All Addons" and "All" options to /random
-- Allow BACAP Bot to be used in DMs and private group chats
-
-=== changelog v1.3 ===
-
-v1.3.1
-- Added the Suggestion Edition pack by Elag422
-- Updated supported BACAP pack version to 1.20.1
-- Updated versions and documentation commands
-
-v1.3.0
-- Added the addon_advancement command, which displays any advancement from any of the BACAP add-ons
-   - Currently, the add-ons available are Terralith Version, Nullscape Version, Torture Edition (by Wolfguy2005), Enhanced Discoveries (by ItzSkyReed and _Fedor_F), Incendium Version (by Ktano2o6o8), Complete Collection (by CoolgirlOmega) and Cereal Dedication (by FixingGlobe)
-- Added an optional argument "addon" to the /random command that if present, picks a random advancement from that add-on instead
-- Updated advancement view to include the advancement ID
-- Added overrides for base pack advancement icons with additional components
-
-=== changelog v1.2 ===
-
-v1.2.1
-- Fixed a bug related to the "Display Trophy" button still lingering when viewing the actual trophy embed
-- Added emojis to advancement embeds and buttons
--- The "More Information" section has been reformatted to be more tidy and clean
-- Reworked button timing out logic - buttons now delete themselves when they time out
--- The "Close" button has been removed with this reworked logic
-- Added a new thumbnail to /doc and /documentation
-
-- i still cannot believe that it's literally not butter
-
-v1.2.0
-- Added capability to read from datapack files
-- Updated advancement view to include criteria count
-- Updated advancement view to include an image of the adv icon on a frame of that adv's category
-- Upgrades, people, upgrades!
-
-=== changelog v1.1 ===
-
-v1.1.5
-- Hotfix refresh_advancements command reloading wrong spreadsheet documentation
-
-v1.1.4
-- Updated versions command to have new bacap versions
-- Updated data to grab from 1.19 documentation
-- Updated doc command to use 1.19 documentation
-
-v1.1.3
-- Added emojis to display name depending on the category of the advancement
-
-v1.1.2
-- Patched in additional adv info again
-- Modified trophy button functionality
-
-v1.1.1
-- Hotfix for images not loading after being loaded the first time
-
-- Updated versions command to include new version bacap 1.18.2
-- Added several thumbnails and pictures for some commands, more will come soon
-- Major internal rework of button logic and handling
--- We are currently aware of a bug with the advancement's button logic where it doesn't time out for some reason (FIXED IN v1.2.1)
-
-- Added logging and removed print statements
-- Made embeds a bit neater with emojis
-- Optimizations here and there
-
-=== changelog v1.0 ===
-
-v1.0r3
-- Fixed being able to run any refresh commands if you're an admin on a server with the bot (thank u p1k0chu)
-
-v1.0r2
-- Reactive messages disabled due to bot spam/abuse (this is why we can't have nice things)
-
-- i still can't believe it's not butter
-=== end of changelog ===
-
-
-© 2025. "The BACAP Bot" All rights reserved.
 '''
 
 # ALL IMPORTS
@@ -2312,3 +2224,4 @@ bot.run(token)
 
 # TWO THOUSAND ONE HUNDRED NINETY FOUR
 
+# TWO THOUSAND TWO HUNDRED TWENTY SEVEN
